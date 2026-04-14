c3676834a81f76e83b1a33a376754cd0e9aff01f
blackeagle686 <mathematecs1@gmail.com>
2026-04-14 14:17:09 +0200

    bug fixed

--- core/config.py
+++ core/config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 class Config:
     REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379")
-    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "ak_2yp3Xw1Ny7ky2pF7er9x93ZO9jj6G")
     OPENAI_BASE_URL = os.getenv("OPENAI_BASE_URL", "https://api.longcat.chat/openai")
     QDRANT_URL = os.getenv("QDRANT_URL", "http://localhost:6333")
 
