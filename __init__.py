d6a0b9103e9617adf138aa6d8251aac2361fdd08
blackeagle686 <mathematecs1@gmail.com>
2026-04-17 18:12:29 +0200

    update

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from .IRYM import init_irym, startup_irym, init_irym_full, get_rag_pipeline, get_insight_engine, get_vlm_pipeline, get_llm, get_memory
+from .IRYM import init_irym, startup_irym, init_irym_full, get_rag_pipeline,set_providers, get_insight_engine, get_vlm_pipeline, get_llm, get_memory
 from .framework.chatbot import ChatBot
 
-__all__ = ["init_irym", "startup_irym", "init_irym_full", "get_rag_pipeline", "get_insight_engine", "get_vlm_pipeline", "get_llm", "get_memory", "ChatBot"]
+__all__ = ["init_irym", "startup_irym", "init_irym_full", "get_rag_pipeline", "set_providers", "get_insight_engine", "get_vlm_pipeline", "get_llm", "get_memory", "ChatBot"]
