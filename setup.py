9be3d3a1c6c1a91868d9bbb89c74555a1dfa41a5
Mohamed Alaa <mathematecs1@gmail.com>
2026-04-29 14:45:03 +0300

    udpate .yml

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="phx-ashborn",
-    version="0.1.3",
+    version="0.1.4",
     packages=find_packages(),
     install_requires=[
         "python-dotenv>=1.0.0",
