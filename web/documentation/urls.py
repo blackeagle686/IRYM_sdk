ffd8063c9593dd47de890e115c00f8d97db79f03
Mohamed Alaa <mathematecs1@gmail.com>
2026-04-29 10:46:53 +0300

    cleaning

--- web/documentation/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('docs/', views.docs, name='docs'),
-    path('docs/<str:doc_name>/', views.docs, name='docs_named'),
-]
