afb96b133d5e4b624130dee70f4c81473535cd1c
blackeagle686 <mathematecs1@gmail.com>
2026-04-30 15:45:58 +0300

    Auto-commit: 2026-04-30 15:45:58

--- phoenix/cognition/thinker.py
+++ phoenix/cognition/thinker.py
@@ -14,8 +14,7 @@ from phoenix.memory.hybrid import HybridMemory
             "file_name": {
                 "file_path": "path/to/file",
                 "task": "edit/append/create",
-            }, 
-            
+            },  
         }, 
         "tasks": {
             "task_name": {
