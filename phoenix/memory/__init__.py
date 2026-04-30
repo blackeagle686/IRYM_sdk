53a3c76d7858c02a231103e9c8129dca07f40e1f
blackeagle686 <mathematecs1@gmail.com>
2026-04-30 16:55:35 +0300

    Auto-commit: 2026-04-30 16:55:35

--- phoenix/memory/__init__.py
+++ phoenix/memory/__init__.py
@@ -1,15 +1,15 @@
-from phoenix.memory.base import BaseMemory
-from phoenix.memory.history import ConversationHistory
-from phoenix.memory.semantic import SemanticMemory
-from phoenix.memory.manager import MemoryManager
-from phoenix.memory.hybrid import HybridMemory
-from phoenix.memory.adapter import InteractiveMemoryAdapter
+from .hybrid.hybrid_manager import HybridMemoryManager as HybridMemory
+from .short_term.stm_manager import ShortTermMemoryManager as ShortTermMemory
+from .long_term.ltm_manager import LongTermMemoryManager as LongTermMemory
+from .semantic.semantic_search import SemanticSearch as SemanticMemory
+from .session import SessionMemory
+from .reflection import ReflectionMemory
 
 __all__ = [
-    "BaseMemory",
-    "ConversationHistory",
-    "SemanticMemory",
-    "MemoryManager",
     "HybridMemory",
-    "InteractiveMemoryAdapter",
+    "ShortTermMemory",
+    "LongTermMemory",
+    "SemanticMemory",
+    "SessionMemory",
+    "ReflectionMemory"
 ]
