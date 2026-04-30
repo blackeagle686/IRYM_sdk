ff4b2e2ad6795f486304c9195c76ba5217f23983
blackeagle686 <mathematecs1@gmail.com>
2026-04-30 15:45:36 +0300

    Auto-commit: 2026-04-30 15:45:36

--- phoenix/cognition/thinker.py
+++ phoenix/cognition/thinker.py
@@ -3,7 +3,7 @@ from phoenix.memory.hybrid import HybridMemory
 
 
 """
-    Thinker must recongize all of the following from user prompt
+    Thinker must recongize all of the following from user prompt.
     semantic cache uset the main_objective to find relevant memory entries to pass back to thinker as context for better understanding of user prompt and workspace state.
     Thinker must return response like this: 
     { 
