bc15481a076d661d4d8da1eb95fb60a20ae49366
blackeagle686 <mathematecs1@gmail.com>
2026-04-30 15:45:21 +0300

    Auto-commit: 2026-04-30 15:45:21

--- phoenix/cognition/thinker.py
+++ phoenix/cognition/thinker.py
@@ -4,6 +4,7 @@ from phoenix.memory.hybrid import HybridMemory
 
 """
     Thinker must recongize all of the following from user prompt
+    semantic cache 
     Thinker must return response like this: 
     { 
         "main_objective": "Refined main goal based on user prompt",
