7b7e6fdd2337c91056b1db576c8fc47c0671f784
blackeagle686 <mathematecs1@gmail.com>
2026-04-30 15:58:05 +0300

    Auto-commit: 2026-04-30 15:58:05

--- phoenix/cognition/thinker.py
+++ phoenix/cognition/thinker.py
@@ -6,10 +6,11 @@ from phoenix.memory.hybrid import HybridMemory
     Thinker must recongize all of the following from user prompt.
     semantic cache uset the main_objective to find relevant memory entries to pass back to thinker as context for better understanding of user prompt and workspace state.
     Thinker in planning mode must generate a tasks files containe on: 
-        {
-            "
-            
-        }
+        [
+            {
+                "task_id"
+            }
+        ]
     Thinker must return response like this: 
     { 
         "main_objective": "Refined main goal based on user prompt",
