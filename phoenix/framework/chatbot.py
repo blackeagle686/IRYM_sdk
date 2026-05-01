2988afe9d023c95be9dffe9f6431a732ee6be60b
blackeagle686 <mathematecs1@gmail.com>
2026-05-01 16:10:08 +0300

    Auto-commit: 2026-05-01 16:10:07

--- phoenix/framework/chatbot.py
+++ phoenix/framework/chatbot.py
@@ -137,7 +137,7 @@ class ChatBotInstance:
 
         # 2. Setup Components
         if self.builder._rag_path:
-            self._rag_pipeline = get_rag_pipeline()
+            self._rag_pipeline = get_rag_pipeline(rag_config=self.builder._rag_config)
             paths = self.builder._rag_path if isinstance(self.builder._rag_path, list) else [self.builder._rag_path]
             for p in paths:
                 logger.info(f"Ingesting: {p}")
