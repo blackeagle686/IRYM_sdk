bfe3bcc45e8f642434cd7fe2f678eef6b21fe9c9
blackeagle686 <mathematecs1@gmail.com>
2026-05-01 19:22:39 +0300

    Bumped version to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="phx-ashborn",
-    version="0.1.9",
+    version="0.2.0",
     packages=find_packages(),
     install_requires=[
         "python-dotenv>=1.0.0",
